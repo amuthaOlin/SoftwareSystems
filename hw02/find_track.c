90a9896c68eea87be8a9d5149903d703b66b0853
Ankeet Mutha <ankeet.mutha@students.olin.edu>
2014-02-17 13:50:14 -0500

    Finished hwk2

--- hw02/find_track.c
+++ hw02/find_track.c
@@ -8,6 +8,7 @@ License: Creative Commons Attribution-ShareAlike 3.0
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
 
@@ -36,10 +37,47 @@ void find_track(char search_for[])
 // Finds all tracks that match the given pattern.
 //
 // Prints track number and title.
-void find_track_regex(char pattern[])
+
+int match(const char *string, char *pattern)
+/*
+ * Match string against the extended regular expression in
+ * pattern, treating errors as no match.
+ *
+ * return 1 for match, 0 for no match
+ */
+
 {
-    // TODO: fill this in
+    int status;
+    regex_t    re;
+
+    if (regcomp(&re, pattern, REG_EXTENDED|REG_NOSUB) != 0) {
+        printf("Did Not Compile Correctly");
+        return(0);      /* report error */
+    }
+    status = regexec(&re, string, (size_t) 0, NULL, 0);
+    regfree(&re);
+    if (status != 0) {
+        return(0);      /* report error */
+
+    }
+    return(1);
 }
+
+void find_track_regex(char pattern[])
+//Uses Regex to find a specifc pattern in the track list
+
+{
+
+    int i;
+    for (i=0; i<NUM_TRACKS; i++) {
+    if (match(tracks[i],pattern)) {
+        printf("Track %i: '%s'\n", i, tracks[i]);
+    }
+    }
+
+
+}
+
 
 // Truncates the string at the first newline, if there is one.
 void rstrip(char s[])
@@ -59,8 +97,8 @@ int main (int argc, char *argv[])
     fgets(search_for, 80, stdin);
     rstrip(search_for);
 
-    find_track(search_for);
-    //find_track_regex(search_for);
+    //find_track(search_for);
+    find_track_regex(search_for);
 
     return 0;
 }
