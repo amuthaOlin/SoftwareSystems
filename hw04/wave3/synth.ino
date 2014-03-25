da54115c2609b46f4def839033dc10cd38c2af94
Ankeet Mutha <ankeet.mutha@students.olin.edu>
2014-03-25 15:12:51 -0400

    Synth final code added

---
+++ hw04/wave3/synth.ino
@@ -0,0 +1,98 @@
+/*
+  
+
+Author: Ankeet, Madison, Allen Downey
+ 
+ Based on http://arduino.cc/en/Tutorial/AnalogInput
+ Created by David Cuartielles
+ modified 30 Aug 2011
+ By Tom Igoe
+
+ License: Public Domain
+ 
+ */
+ 
+ 
+int ledPin = 5;       // select the pin for the LED
+int buttonPin1 = 2;
+int buttonPin2 = 3;
+int prevButton1 = 1;
+int prevButton2 = 1;
+
+int notes[7] = {220, 246, 261, 293, 329, 349, 391};
+int note = 2;
+float conversion = 0.46;
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(buttonPin1, INPUT_PULLUP);  
+  pinMode(buttonPin2, INPUT_PULLUP);  
+
+  pinMode(ledPin, OUTPUT);
+  
+  pinMode(13, OUTPUT);  
+  pinMode(12, OUTPUT);  
+  pinMode(11, OUTPUT);  
+  pinMode(10, OUTPUT);  
+  pinMode(9, OUTPUT);  
+  pinMode(8, OUTPUT);  
+  pinMode(7, OUTPUT);  
+  pinMode(6, OUTPUT);  
+  
+  // timer1
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 240;
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS12 and CS10 bits for 64 prescaler
+  TCCR1B |= (1 << CS11) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+  
+  sei();
+  
+  attachInterrupt(0, increment, RISING);  // Interrupt attached to the button connected to pin 2
+  attachInterrupt(1, decrement, RISING); 
+}
+
+
+void writeByte(int x) {
+  int pin;
+  
+  //PORTD: 0 - 7
+  //PORTB: 8 - 13
+  PORTD = ~(x << 6);
+  PORTB = ~(x >> 2);
+}
+
+int low = 36;
+int high = 255;
+int stride = 5;
+int counter = low;
+
+ISR(TIMER1_COMPA_vect){
+  counter += stride;
+  if (counter > high) {
+    counter = low;
+  }
+
+  // write to the digital pins  
+  writeByte(counter);
+}
+
+void increment() {
+  note++;
+  OCR1A = (int) (conversion * notes[note%7]);
+}
+
+void decrement() {
+  note--;
+  OCR1A = (int) (conversion * notes[note%7]);
+}
+
+void loop() {
+}
